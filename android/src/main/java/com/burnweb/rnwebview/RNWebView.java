9cf5dd155db6e50312abc2a9e59a83dd106b7367
Jacob Barr <kobi.bar@onoapps.com>
2018-07-12 16:15:47 +0300

    Enabled opening javascript windows automatically.

--- android/src/main/java/com/burnweb/rnwebview/RNWebView.java
+++ android/src/main/java/com/burnweb/rnwebview/RNWebView.java
@@ -107,6 +107,7 @@ class RNWebView extends WebView implements LifecycleEventListener {
         this.getSettings().setLoadsImagesAutomatically(true);
         this.getSettings().setBlockNetworkImage(false);
         this.getSettings().setBlockNetworkLoads(false);
+        this.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             this.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
